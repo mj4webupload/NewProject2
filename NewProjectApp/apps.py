e6401ff1734f55e60e8f14d0d360ed5bfbb5d8f4
mj4webupload <mj4webupload@gmail.com>
2022-05-21 17:13:47 +0530

    message

---
+++ NewProjectApp/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class NewprojectappConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'NewProjectApp'
---
+++ NewProjectApp/urls.py
@@ -0,0 +1,6 @@
+import imp
+from .views import index
+from django.urls import path
+urlpatterns = [
+    path('',index),
+]
